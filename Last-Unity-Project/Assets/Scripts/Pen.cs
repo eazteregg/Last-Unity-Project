0a7fb61902aeedbe390bc14e1cdc76e42e405da5
Henning Stegemann <henning.stegemann@web.de>
2019-06-13 14:47:47 +0200

    Painting demo finished, still needs to be adjusted for VR

---
+++ Last-Unity-Project/Assets/Scripts/Pen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Pen : VRTK_InteractableObject
+{
+    public Painting painting;
+    private RaycastHit touch;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.painting = GameObject.Find("Painting").GetComponent<Painting>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 tip = transform.Find("Tip").transform.position;
+        
+        if (Physics.Raycast(tip, transform.up, out touch, distance))
+        {
+            if (!(touch.collider.tag == "Painting"))
+                return;
+
+            this.painting = touch.collider.GetComponent < Painting > ();
+            Debug.Log("touching!");
+
+            this.painting.SetColor(Color.black);
+            this.painting.SetTouchPosition(touch.textureCoord.x, touch.textureCoord.y);
+            this.painting.ToggleTouch(true);
+
+        } else
+        {
+            this.painting.ToggleTouch(false);
+        }
+    }
+}
