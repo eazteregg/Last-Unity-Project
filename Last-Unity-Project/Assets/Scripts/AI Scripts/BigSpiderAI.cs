63aa18fe824f8659ccf0c4ee1d73bb9977d2cccb
MISSEY <skmgun@gmail.com>
2019-06-20 17:36:47 +0200

    Implement Intelligent Enemy

---
+++ Last-Unity-Project/Assets/Scripts/AI Scripts/BigSpiderAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigSpiderAI : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public float TargetDistance;
+    public GameObject TheEnemy;
+    public float EnemySpeed;
+    public int AttackTrigger;
+    public int DealingDamage;
+    public float AllowedRange = 80;
+    public RaycastHit Shot;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(ThePlayer.transform);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if (TargetDistance <= AllowedRange)
+            {
+                EnemySpeed = 0.05f;
+                if (AttackTrigger == 0)
+                {
+                    TheEnemy.GetComponent<Animation>().Play("Run");
+                    transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, EnemySpeed);
+                }
+            }
+            else
+            {
+                EnemySpeed = 0;
+                TheEnemy.GetComponent<Animation>().Play("Idle");
+            }
+        }
+        
+        if (AttackTrigger == 1)
+        {
+                TheEnemy.GetComponent<Animation>().Play("Attack");
+        }
+
+    }
+   
+
+    void OnTriggerEnter()
+    {
+        AttackTrigger = 1;
+    }
+
+    void OnTriggerExit()
+    {
+        AttackTrigger = 0;
+    }
+}
---
+++ Last-Unity-Project/Assets/Scripts/EnemyScripts/InflictDamage.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Inflict Damage Script:
+
+public class InflictDamage : MonoBehaviour
+{
+
+    public int DamageAmount = 5;
+    public float TargetDistance;
+    public float AllowedRange = 2.7f;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                TargetDistance = hit.distance;
+                if (TargetDistance <= AllowedRange)
+                {
+                    hit.transform.SendMessage("DeductPoints", DamageAmount, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+
+}
+
+
+//===================
+
+//SpiderEnemy
+
+public class SpiderEnemy : MonoBehaviour
+{
+
+    public int EnemyHealth = 10;
+
+    void DeductPoints(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+    }
+
+    void Update()
+    {
+        if (EnemyHealth <= 0)
+        {
+            StartCoroutine(DeathSpider());
+        }
+    }
+
+    IEnumerator DeathSpider()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}
