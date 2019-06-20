d3fdf816dfba761b6f2add2a97600563d675e8f3
Richard <rruppel@uni-osnabrueck.de>
2019-06-20 17:57:56 +0200

    Proto Scene Build
    Proto room with built objects in hierarchy

---
+++ Last-Unity-Project/Assets/Destructible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+ public GameObject destroyedVersion;
+
+ void OnMouseDown()
+ {
+   //spawn shattered Object
+   Instantiate(destroyedVersion, transform.position, transform.rotation);
+   //Remove whole object
+   Destroy(gameObject);
+ }
+}
